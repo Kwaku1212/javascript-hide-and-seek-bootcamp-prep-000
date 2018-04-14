9f4087651add64af212f82638e794226103f921f
Kwaku J <gyazi@live.com>
2018-04-14 17:54:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector(selector){
+  var query = document.querySelector(selector)
+  return query
+}
+
+function nestedTarget(){
+  var query = document.getElementById('nested').querySelector('.target')
+  return query
+}
+
+function increaseRankBy(n){
+  var query = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(var i = 0; i < query.length; i++){
+    query[i].innerHTML = (parseInt(query[i].innerHTML) + n ) 
+  }
+}
+
